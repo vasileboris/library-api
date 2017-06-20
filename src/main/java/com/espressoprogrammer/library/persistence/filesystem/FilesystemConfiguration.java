a8dbc9a0364822359c625aec408a16c6c3d4952f
vasile boris <boris@espressoprogrammer.com>
2017-06-20 07:15:34 +0300

    Default libraryFolder to HOME/Library

--- src/main/java/com/espressoprogrammer/library/persistence/filesystem/FilesystemConfiguration.java
+++ src/main/java/com/espressoprogrammer/library/persistence/filesystem/FilesystemConfiguration.java
@@ -2,14 +2,18 @@ package com.espressoprogrammer.library.persistence.filesystem;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class FilesystemConfiguration {
 
-    @Value("${filesystem.dao.rootfolder}")
+    @Value(value = "${filesystem.dao.rootfolder:}")
     private String libraryFolder;
 
     public String getLibraryFolder() {
-        return libraryFolder;
+        if(!StringUtils.isEmpty(libraryFolder)) {
+            return libraryFolder;
+        }
+        return System.getProperty("user.home") + "/Library";
     }
 }
